8ffa4055813f9214ae1d3a9fbf59c63d2e2685d9
Yao Ming Huang <yaoming.huang@ischool.com.tw>
2018-05-08 22:17:37 +0800

    寫作業。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,25 @@ export class AppComponent {
   title = 'app';
   url = 'http://blog.miniasp.com/';
   imgurl = '/assets/images/logo.png';
+
+  pattern = '';
+  characterCount = 0;
+
   constructor() {
   }
   changeTitle(altKey: boolean) {
     if (altKey) {
       this.title  = 'The Will Will Web';
+    }
+  }
+
+  updateValue($event: any) {
+    this.pattern = $event.target.value;
+    this.characterCount = this.pattern.length;
+
+    if ($event.key === 'Escape') {
+      this.pattern = '';
+      this.characterCount = 0;
     }
   }
 }
